c1d7e5f00986f6a1831a99975c9939ba8fd5e91e
Dwayne Man <dwayne.man@gmail.com>
2022-10-06 02:55:27 +0800

    Products list: ProductHomeItem component. Product flatlist rendering products.

--- src/screens/app/home/styles.js
+++ src/screens/app/home/styles.js
@@ -2,13 +2,16 @@ import { StyleSheet } from "react-native"
 
 const styles = StyleSheet.create({
     container: {
-        padding: 24
+        marginVertical: 24
     },
     title: {
         fontSize: 16
     },
     flatList: {
         paddingVertical: 24
+    },
+    productsFlatList: {
+        paddingHorizontal: 16
     }
 })
 
